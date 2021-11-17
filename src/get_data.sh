72c749d98fc7b6336343ef165fabc8374a43bf33
simLacombe <simon.lacombe@ens-lyon.fr>
2021-11-17 13:57:19 +0000

    debug

--- src/get_data.sh
+++ src/get_data.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
 cd ~/mydatalocal/tp_NGS_Simon
-srr =("SRR8257100" "SRR8257106")
+srr=("SRR8257100" "SRR8257106") #SRR files (WT, mutant)
 
-for sample in $srr
+for sample in $srr #get data
 do
   echo $sample
   parallel-fastq-dump --sra-id $sample \
     --threads 8 --outdir data/ \
-    --split-files --gzip
+    --split-files --gzip 
 done
