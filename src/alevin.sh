3f3b104cbc56ae502344845c786db862db197707
simLacombe <simon.lacombe@ens-lyon.fr>
2021-11-18 13:36:03 +0000

    add tgmap building specify path in a var

--- src/alevin.sh
+++ src/alevin.sh
@@ -3,12 +3,19 @@
 cd ~/mydatalocal/tp_NGS_Simon
 
 SRR="SRR8257100 SRR8257106"
+data_path="data/transcriptome_data/"
 
+###Build salmon index
+salmon index -t ${data_path}Arabidopsis_transcriptome.fa -i ${data_path}transcriptome_salmon_index -k 31 -p 8
 
-salmon index -t data/Arabidopsis_transcriptome.fa -i data/transcriptome_salmon_index -k 31 -p 8
+###Build tgmap
+bioawk -c gff '$feature=="exon" {print $9}' < ${data_path}Arabidopsis_transcriptome.gtf \
+  | awk -F " " '{print substr($2,2,length($2)-3) "\t" substr($4,2,length($4)-3)}' - > ${data_path}txp2gene.tsv
+  
+###Run alevin
 for sample in $SRR
 do
-salmon alevin -l ISR -1 data/${sample}_1.fastq.gz -2 data/${sample}_2.fastq.gz --chromium\
-  -i data/transcriptome_salmon_index -p 8 -o results/output_alevin\
-  --tgmap XXX
+salmon alevin -l ISR -1 data/sample_data/${sample}_1.fastq.gz -2 data/sample_data/${sample}_2.fastq.gz --chromium\
+  -i ${data_path}transcriptome_salmon_index -p 8 -o results/output_alevin_${sample}\
+  --tgMap ${data_path}txp2gene.tsv
 done
